50ff1b01d29be9c4018479c742cae0840d37073b
Новиков Евгений Алексеевич <eanovikov@phoenixit.ru>
2022-10-09 18:24:12 +0300

    @feature finish 4 lab

--- SRW4/Form1.cs
+++ SRW4/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using static System.Math;
 
 namespace SRW4
 {
@@ -15,6 +11,42 @@ namespace SRW4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var y = CalculateResult();
+            ShowResult(y);
+        }
+
+        private double CalculateResult()
+        {
+            var a = Convert.ToInt32(textBox1.Text);
+            var b = Convert.ToInt32(textBox2.Text);
+            var c = Convert.ToInt32(textBox4.Text);
+            var x = Convert.ToInt32(textBox3.Text);
+            return Sqrt(a + Sqrt(b + Sqrt(c + x)));
+        }
+
+        private void ShowResult(double y)
+        {
+            DialogResult result = MessageBox.Show("Y=" + Convert.ToString(y),
+                "Результат:", MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+            switch (result)
+            {
+                case DialogResult.Yes:
+                    textBox1.BackColor = Color.Green;
+                    break;
+                case DialogResult.No:
+                    label2.BackColor = Color.Red;
+                    break;
+                case DialogResult.Cancel:
+                    button1.BackColor = Color.Yellow; 
+                    break;
+                default:
+                    throw new InvalidOperationException("Недопустимая операция");
+            }
         }
     }
 }
