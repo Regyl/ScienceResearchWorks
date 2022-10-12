f586642039b6ede94a500bb6ef9226175824f1a5
Новиков Евгений Алексеевич <eanovikov@phoenixit.ru>
2022-10-12 06:57:14 +0300

    @feature finish 5 lab

--- SRW5/Form1.Designer.cs
+++ SRW5/Form1.Designer.cs
@@ -29,11 +29,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(276, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Результат:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(260, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(276, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(260, 405);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(276, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(188, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "При y=";
+            // 
+            // label2
+            // 
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(172, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "При b1=";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(260, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(276, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(172, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "При b2=";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(260, 160);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(276, 22);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(172, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "При b3=";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(260, 197);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(276, 22);
+            this.textBox5.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(802, 458);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Button button1;
 
         #endregion
     }
--- SRW5/Form1.cs
+++ SRW5/Form1.cs
@@ -1,20 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace SRW5
 {
     public partial class Form1 : Form
     {
+        private const string ErrorResponse = "неопред.знач.";
         public Form1()
         {
             InitializeComponent();
+            InitializeVariables();
+        }
+
+        private void InitializeVariables()
+        {
+            textBox1.Text = Interaction.InputBox("Input y", "Data Input", "0");
+            textBox3.Text = Interaction.InputBox("Input b1", "Data Input", "0");
+            textBox4.Text = Interaction.InputBox("Input b2", "Data Input", "0");
+            textBox5.Text = Interaction.InputBox("Input b3", "Data Input", "0");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double r = Calculate();
+                ShowResult(r, false);
+            }
+            catch (ArgumentException)
+            {
+                ShowResult(0, true);
+            }
+        }
+
+        private double Calculate()
+        {
+            double r;
+            var y = Convert.ToInt32(textBox1.Text);
+            var b1 = Convert.ToInt32(textBox3.Text);
+            var b2 = Convert.ToInt32(textBox4.Text);
+            var b3 = Convert.ToInt32(textBox5.Text);
+
+            if ((y - 0.5 * b1) > 0 && y > 25)
+            {
+                r = Math.Pow(y, 3) - 0.5 * b1;
+            } else if (y <= 25)
+            {
+                r = Math.Pow(Math.Sin(b2), 2) + Math.Abs(b3);
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+            return r;
+        }
+
+        private void ShowResult(double r, bool error)
+        {
+            Color color;
+            if (error)
+            {
+                textBox2.Text = ErrorResponse;
+                color = Color.Red;
+            }
+            else
+            {
+                if (r < 0)
+                {
+                    color = Color.Red;
+                }
+                else
+                {
+                    color = Color.Blue;
+                }
+                textBox2.Text = Convert.ToString(r); 
+            }
+
+            textBox2.BackColor = color;
         }
     }
 }
